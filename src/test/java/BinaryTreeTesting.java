d4bf4f3fed475d05936789fadd2a5bcae0cccba1
DCUPM <daniel.caleiras.martin@alumnos.upm.es>
2022-11-14 12:33:52 +0100

    Agregado el Iterator a las pruebas

--- src/test/java/BinaryTreeTesting.java
+++ src/test/java/BinaryTreeTesting.java
@@ -3,7 +3,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.swing.text.html.HTMLDocument;
+
+import java.util.Iterator;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BinaryTreeTesting {
@@ -39,5 +44,25 @@ public class BinaryTreeTesting {
         System.out.println("*** prueba equals(BinaryTree<T> tree) ***");
     }
 
+    @Test
+    @DisplayName("Probando remove(Node<T> node)")
+    public void removeTesting() {
+        bt.remove(bt.search("6"));
+        assertEquals(6, bt.size());
+        System.out.println("Despues de remove 6...");
+        System.out.print(bt.toString());
+        System.out.println("Probando remove(Node<T> node)");
+        assertThrows(Exception.class, () -> {bt.remove(bt.search("0"));});
+        System.out.println("Despues de remover 0...");
+        System.out.print(bt.toString());
+    }
 
+    @Test
+    @DisplayName("Probando iterator()")
+    public void iteratorTesting() {
+        Iterator it = bt.iterator();
+        System.out.println("*** Probando iterator() ***");
+        while (it.hasNext())
+            System.out.print(it.next().toString()+" ");
+    }
 }
